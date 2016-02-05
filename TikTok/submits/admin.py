5ef7065f165db570516e4668f123dbf6726b45d7
Andrzej Golonka <WiecznyWem@gmail.com>
2016-02-05 15:41:22 +0100

    folder

---
+++ TikTok/submits/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Submit
+
+class SubmitAdmin(admin.ModelAdmin):
+	#fields = [ 'user']
+	list_display = ('problem', 'user', 'pub_date', 'is_executing', 'executed', 'accomplishment')
+	list_filter = ['problem', 'user', 'pub_date', 'is_executing', 'executed', 'accomplishment']
+	search_fields = ['code', 'output', 'comment']
+
+admin.site.register(Submit, SubmitAdmin)
