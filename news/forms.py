5ad8773f7bb8bd29ffce2ac04a4656e7990d6213
Andrzej Golonka <WiecznyWem@gmail.com>
2016-02-05 15:44:11 +0100

    folder2

--- news/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class NewsForm(forms.Form):
-    content = forms.CharField(label='Treść', widget=forms.Textarea)
-
--- submits/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SubmitsConfig(AppConfig):
-    name = 'submits'
