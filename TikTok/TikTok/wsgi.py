d2bb511c52f61f4de0aa183d7b0b70c532f86f8c
Andrzej Golonka <WiecznyWem@gmail.com>
2016-02-14 09:10:25 +0100

    naprawa bledu 500 z apache

--- TikTok/TikTok/wsgi.py
+++ TikTok/TikTok/wsgi.py
@@ -1,6 +1,9 @@
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "/../")
+sys.path.append(dir)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TikTok.settings")
 
